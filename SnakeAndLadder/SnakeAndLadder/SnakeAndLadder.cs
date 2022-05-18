2c2014f79fed62df5a7c74dd581a854f1b01fd39
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-18 12:37:33 +0530

    [chinki] snake and ladder game single player start position 0

---
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+       const int START_POSITION = 0;
+       const int END_POSITION = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start position is: " + START_POSITION);
+            Console.WriteLine("End postion is: " + END_POSITION);
+
+               
+            
+        }
+    }
+}
