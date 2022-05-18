26860b9c447623da8999f1fbeeb8a2f9f87a599b
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-18 13:07:21 +0530

    [chinki] In snake and ladder game roll Die get number between 1_to_7

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -10,11 +10,20 @@ namespace SnakeAndLadder
     {
        const int START_POSITION = 0;
        const int END_POSITION = 100;
+       public static int currentPosition = 0;
+        public static int throwDice()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 7);
+            return number;
+        }
         static void Main(string[] args)
         {
+            currentPosition += throwDice();
+            Console.WriteLine("current Position is:" + currentPosition);
             Console.WriteLine("Start position is: " + START_POSITION);
-            Console.WriteLine("End postion is: " + END_POSITION);      
-            
+            Console.WriteLine("End postion is: " + END_POSITION);
+
         }
     }
 }
