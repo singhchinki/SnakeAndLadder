2b2986cdeaec7597a3167cc591bf8f8a45e9d5d4
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-18 13:58:56 +0530

    [chinki] Player play the Game after checks for Option

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -11,18 +11,43 @@ namespace SnakeAndLadder
        const int START_POSITION = 0;
        const int END_POSITION = 100;
        public static int currentPosition = 0;
+        public static int selectOption = 0;
+       static Random random = new Random();
         public static int throwDice()
         {
-            Random random = new Random();
-            int number = random.Next(1, 7);
-            return number;
+            return random.Next(1, 7);
+        }
+        public static int checkForOption()
+        {
+            return random.Next(1,4);
+        }
+        public static int playerOption()
+        {
+            int throwDices = throwDice();
+            int selectOption = checkForOption();
+            if (selectOption == 2)
+            {
+                currentPosition += throwDices;
+            }
+            else if (selectOption == 3)
+            {
+                currentPosition -= throwDices;
+                if (currentPosition < 0)
+                {
+                    currentPosition = START_POSITION;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do nothing player stay in same position");
+            }
+            return currentPosition;
         }
         static void Main(string[] args)
         {
-            currentPosition += throwDice();
-            Console.WriteLine("current Position is:" + currentPosition);
             Console.WriteLine("Start position is: " + START_POSITION);
             Console.WriteLine("End postion is: " + END_POSITION);
+            Console.WriteLine("Current position is: " + playerOption());
 
         }
     }
